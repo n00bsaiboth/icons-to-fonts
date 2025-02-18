64e8a7095015758ad5bbc2f8bec42a9b63d7aa3e
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2025-02-18 19:20:45 +0200

    develop: setting up some example content

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<?php 
+    require '../vendor/autoload.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Icons to Fonts</title>
+</head>
+<body>
+    <h1>Welcome to icons to fonts</h1>
+    <?php require_once('src/database.php'); ?>
+</body>
+</html>
