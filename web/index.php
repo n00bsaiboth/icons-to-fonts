dd12da8366b4a8f9076206c4bbe7b22984b547c3
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2025-02-19 15:13:27 +0200

    develop: test run

--- web/index.php
+++ web/index.php
@@ -7,10 +7,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="build/css/style.css">
     <title>Icons to Fonts</title>
 </head>
 <body>
     <h1>Welcome to icons to fonts</h1>
-    <?php require_once('src/database.php'); ?>
+    <p><?php require_once('src/database.php'); ?></p>
+
+    <h2>Social media icons: </h2>
+    
+    <p>
+        <span class="icon icon-bluesky"></span>
+        <span class="icon icon-facebook"></span>
+        <span class="icon icon-github"></span>
+        <span class="icon icon-linkedin"></span>
+        <span class="icon icon-mastodon"></span>
+        <span class="icon icon-reddit"></span>
+    </p>
+
+    <script src="build/js/bundle.js"></script>
 </body>
 </html>
