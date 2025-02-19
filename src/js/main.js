16e61e7b498e4e66787b8cda1c578d6c74a4f68c
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2025-02-19 14:00:16 +0200

    develop: setting up webpack

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+import '../scss/style.scss';
+
+console.log(`Hello from webpack!`);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'web/build/js'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/style.css',
+        })
+    ],
+}
